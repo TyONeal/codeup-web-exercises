ba7577dd3f505831325bd1e3b339a380fc67c736
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-06-09 11:38:09 -0500

    added welcome.html doc

---
+++ js/for_loops.js
@@ -0,0 +1,9 @@
+"use strict"
+
+Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+function showMultiplicationTable(num) {
+    return (num * 1);
+}
+
+
