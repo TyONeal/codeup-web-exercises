e88ad3bfa52ce43cca169cfdaee8bef44072ce8e
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-06-12 17:42:57 -0500

    adding additional exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -23,13 +23,32 @@ console.log(randomInt);
 
 function numberIsEven(input) {
     if (input % 2 === 0) {
-        console.log("Your number is even");
-        return true
+        return "even."
     } else {
-        console.log("Your number is odd.");
-        return false
+        return "odd."
     }
 
 }
 
 console.log(numberIsEven(randomInt));
+
+for (let i = 1; i < 11; i++) {
+
+    const randomNumber = forRandomNumbers(20, 200);
+    console.log("in Iteration " + i + " the random number is " + randomNumber + " which is " + numberIsEven(randomNumber))
+
+    }
+
+
+for (let i = 1; i <= 9; i++) {
+    let res = [];
+    for (let a = 1; a <= i; a++) {
+        res.push(i);
+    }
+    console.log(res.join(""));
+}
+
+let x = 100;
+for (let i = 0; i <= 19; i++) {
+    console.log(x - 5);
+}
