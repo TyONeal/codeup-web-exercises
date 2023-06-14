af67c88859f24eb9db4b70e9056ebee35f77a762
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-06-14 09:13:00 -0500

    css selectors exercise

--- js/while.js
+++ js/while.js
@@ -17,10 +17,10 @@ while (i < 65537) {
 
 
 // This is how you get a random number between 50 and 100
-let allCones = Math.floor(Math.random() * 50) + 50;
+const allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-
+    console.log(allCones);
       do {
          let conesSold = Math.floor(Math.random() * 5) + 1;
-          console.log(allCones - conesSold);
+          console.log(allCones);
       } while (allCones === 0);
