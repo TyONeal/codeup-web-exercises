be24bd1da24042d19c3cd45103d0bb3d7dd05922
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-10 11:06:48 -0500

    jquery event exercises

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,19 +1,41 @@
-"use strict"
+ "use strict"
+//
+//
+// $(function() {
+//
+//     alert($('#id-example-1').html());
+//
+//     $('.code-up')
+//         .css('border','1px solid red');
+//
+//     $('li').css("font-size", "20px");
+//
+//     $('h1, p, li').css("background-color", "yellow");
+//
+//     alert($('h1').html());
+//
+//
+// });
+
+ $(function () {
+
+     $('h1').click(function() {
+         $('h1').css("backgroundColor", "red")
+
+     });
+
+     $('p').dblclick(function() {
+        $('p').css("font-size", '18px')
+     });
+
+     $('li').mouseenter(function() {
+         $(this).css("color", "red")
+             .mouseleave(function() {
+                 $(this).css("color", "black")
+             });
+
+     });
 
 
-$(function() {
-
-    alert($('#id-example-1').html());
-
-    $('.code-up')
-        .css('border','1px solid red');
-
-    $('li').css("font-size", "20px");
-
-    $('h1, p, li').css("background-color", "yellow");
-
-    alert($('h1').html());
-
-
-});
+ });
 
