104dbc644968050b02660c23eb8e520f840d6a0e
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-12 09:02:56 -0500

    working through konami project. made changes

---
+++ js/konami.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//make the inputs into an array
+
+
+//write a function that takes in the correct order of inputs
+
+const correctInput = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+
+//if function takes in correct inputs, do something
+const submitting = document.querySelector('#submit-button')
+const userTyping = document.querySelector('#user-input')
+document.addEventListener('keyup', (e) => {
+
+    });
+
