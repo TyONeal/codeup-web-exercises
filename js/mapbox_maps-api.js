34b79e86feab56200811613fcd593bd9a44d9f28
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-13 16:06:51 -0500

    started mapbox exercise

---
+++ js/mapbox_maps-api.js
@@ -0,0 +1,60 @@
+$(() => {
+   //variables
+    const map = initializeMap();
+
+
+
+
+
+
+    //functions
+
+    function initializeMap() {
+
+        mapboxgl.accessToken = MAPBOX_TOKEN;
+
+        const mapOptions = {
+            container: 'map',
+            style: 'mapbox://styles/mapbox/navigation-night-v1',
+            zoom: 15,
+            center: [-98.4916, 29.4252],
+
+        }
+        return new mapboxgl.Map(mapOptions);
+    }
+
+
+
+
+
+
+    //events
+
+
+
+
+
+    //perform at load
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
