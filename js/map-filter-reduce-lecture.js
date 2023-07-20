b4f798e1cb75f02c55764e611afeb44d6b0ddb7c
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-20 13:21:28 -0500

    map-filter-reduce exercise

---
+++ js/map-filter-reduce-lecture.js
@@ -0,0 +1,49 @@
+(() => {
+
+
+    //map
+    const url ='./data/houses.json'
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch(url, options)
+        .then(response => {
+            return response.json();
+        })
+        .then(houses => {
+            console.log(houses);
+            let housesTwoBedsPlus = houses.filter(house => {
+               const nmrOfBaths = parseFloat(house.baths)
+                return parseFloat(house.beds) >= 5 && nmrOfBaths >= 2;
+
+            });
+                console.log(housesTwoBedsPlus);
+
+                //map method
+
+            let housesPriceRange = houses.map(house => {
+                const priceRange = 200_000;
+                const canBuy = parseFloat(house.price) > priceRange ? 'Nope' : 'Yes!'
+                const newValue = `
+                        Address: ${house.address},
+                        Can I afford it: ${canBuy}`
+
+                return newValue;
+            });
+
+            for(let house of housesPriceRange) {
+                console.log(house);
+            }
+
+
+
+
+        });
+
+
+
+
+})();
---
+++ js/map-filter-reduce.js
@@ -0,0 +1,91 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+//Problem 2
+
+const languageUsers = users.filter((user) => {
+        return user.languages.length >= 3
+
+
+
+});
+    console.log(languageUsers);
+
+//Problem 3
+
+const getEmailsFromArray = users.map((user) => {
+   return user.email
+});
+
+console.log(getEmailsFromArray);
+
+// Problem 4
+
+const totalExperience = users.reduce((totalExperience, user) => {
+    return totalExperience + user.yearsOfExperience
+
+}, 0);
+
+console.log(totalExperience);
+
+const averageExperience = totalExperience / 5;
+
+console.log(averageExperience);
+
+//Problem 5
+
+const longestEmail = users.reduce((longestEmail, user) => {
+        if (user.email.length > longestEmail.length) {
+            longestEmail = user.email
+        }
+        return longestEmail;
+}, '');
+
+
+
+console.log(longestEmail)
+
+//Problem 6 ??? Having trouble getting this to show up as a single string...
+
+const listOfInstructors = users.reduce ((listOfNames, user) => {
+       return listOfNames += ` ${user.name}`;
+
+}, 'Your instructors are:');
+
+console.log(listOfInstructors);
+
