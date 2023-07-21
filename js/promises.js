4d2bfb4dbdab4596a9f4125e2482b07c74c3ec94
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-21 15:31:45 -0500

    refactored promises exercise with async shorthand

--- js/promises.js
+++ js/promises.js
@@ -1,29 +1,25 @@
-const getUser = (username) => {
+const getUser = async(username) => {
     const url = `https://api.github.com/users/${username}/events`;
     const options = {headers: {'Authorization': PROMISES_TOKEN}}
 
 
-    return fetch(url, options).then((response) => {
-        return response.json();
+    const response = await fetch(url, options)
+    const data = await response.json();
+                return data;
 
-    })
 }
 
-(() => {
+(async() => {
     let textSubmit = document.querySelector('.text-box');
-    textSubmit.addEventListener('keyup', (e) => {
+    textSubmit.addEventListener('keyup', async(e) => {
+        console.log(e.code);
         if (e.code === 'Enter') {
-            getUser(textSubmit.value).then((userData)=> {
-                console.log(userData);
-                alert(userData[0].created_at);
-            })
+            const userSearch = await getUser(textSubmit.value);
+                const filteredSearch = userSearch.filter(pushEvent => {
+                    return pushEvent.type === 'PushEvent';
+                })
+                console.log(filteredSearch[0].created_at);
+                alert(filteredSearch[0].payload.commits[0].message);
         }
     })
-
-
-
-
-
-
-
 })();
